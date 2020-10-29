e4b1591dcb3bc5d93e11f357af0ef248b1fc711e
nvsquirrel <61885402+nvsdm136@users.noreply.github.com>
2020-10-28 17:58:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,59 +20,57 @@ mkdir build; cd build
 cmake -DENABLE_LYD_PRIV=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr \
       -D CMAKE_BUILD_TYPE:String="Release" ..
 make
-sudo make install
+make install
 
 
 printf "######################################\n####  libyang has been installed  ####\n######################################\n"
 
 
 
-sudo groupadd -g 92 frr
-sudo groupadd -r -g 85 frrvty
-sudo useradd -u 92 -g 92 -M -r -G frrvty -s /sbin/nologin -c "FRR FRRouting suite" -d /var/run/frr frr
+groupadd -g 92 frr
+groupadd -r -g 85 frrvty
+useradd -u 92 -g 92 -M -r -G frrvty -s /sbin/nologin -c "FRR FRRouting suite" -d /var/run/frr frr
 cd ../../
 git clone --branch "stable/7.3" https://github.com/frrouting/frr.git frr
 cd frr/
 sed -i '/^AC_CONFIG_MACRO_DIR*/a AC_CONFIG_AUX_DIR([.])' configure.ac
 . ./bootstrap.sh
-#./bootstrap.sh
-#pwd
 sh configure --bindir=/usr/bin --sbindir=/usr/lib/frr --sysconfdir=/etc/frr --libdir=/usr/lib/frr --libexecdir=/usr/lib/frr --localstatedir=/var/run/frr --with-moduledir=/usr/lib/frr/modules --enable-snmp=agentx --enable-multipath=64 --enable-user=frr --enable-group=frr --enable-vty-group=frrvty --enable-systemd=yes --disable-exampledir --disable-ldpd --enable-fpm --with-pkg-git-version --with-pkg-extra-version=-MyOwnFRRVersion SPHINXBUILD=/usr/bin/sphinx-build
 make
 make check
-sudo make install
+make install
 
 
 printf "########################################\n####  FRR Installation is Complete  ####\n########################################\n"
 
 
 
-sudo mkdir /var/log/frr
-sudo mkdir /etc/frr
-sudo touch /etc/frr/zebra.conf
-sudo touch /etc/frr/bgpd.conf
-sudo touch /etc/frr/ospfd.conf
-sudo touch /etc/frr/ospf6d.conf
-sudo touch /etc/frr/isisd.conf
-sudo touch /etc/frr/ripd.conf
-sudo touch /etc/frr/ripngd.conf
-sudo touch /etc/frr/pimd.conf
-sudo touch /etc/frr/nhrpd.conf
-sudo touch /etc/frr/eigrpd.conf
-sudo touch /etc/frr/babeld.conf
-sudo chown -R frr:frr /etc/frr/
-sudo touch /etc/frr/vtysh.conf
-sudo chown frr:frrvty /etc/frr/vtysh.conf
-sudo chmod 640 /etc/frr/*.conf
-sudo install -p -m 644 tools/etc/frr/daemons /etc/frr/
-sudo chown frr:frr /etc/frr/daemons
+mkdir /var/log/frr
+mkdir /etc/frr
+touch /etc/frr/zebra.conf
+touch /etc/frr/bgpd.conf
+touch /etc/frr/ospfd.conf
+touch /etc/frr/ospf6d.conf
+touch /etc/frr/isisd.conf
+touch /etc/frr/ripd.conf
+touch /etc/frr/ripngd.conf
+touch /etc/frr/pimd.conf
+touch /etc/frr/nhrpd.conf
+touch /etc/frr/eigrpd.conf
+touch /etc/frr/babeld.conf
+chown -R frr:frr /etc/frr/
+touch /etc/frr/vtysh.conf
+chown frr:frrvty /etc/frr/vtysh.conf
+chmod 640 /etc/frr/*.conf
+install -p -m 644 tools/etc/frr/daemons /etc/frr/
+chown frr:frr /etc/frr/daemons
 sed -i "s/bgpd=.*/bgpd=yes/g" /etc/frr/daemons
 printf "# Sysctl for routing\n#\n# Routing: We need to forward packets\nnet.ipv4.conf.all.forwarding=1\nnet.ipv6.conf.all.forwarding=1" > /etc/sysctl.d/90-routing-sysctl.conf
-sudo sysctl -p /etc/sysctl.d/90-routing-sysctl.conf
-sudo install -p -m 644 tools/frr.service /usr/lib/systemd/system/frr.service
-sudo systemctl preset frr.service
-sudo systemctl enable frr
-sudo systemctl enable strongswan
+sysctl -p /etc/sysctl.d/90-routing-sysctl.conf
+install -p -m 644 tools/frr.service /usr/lib/systemd/system/frr.service
+systemctl preset frr.service
+systemctl enable frr
+systemctl enable strongswan
 cd ../
 cat bgpd.conf > /etc/frr/bgpd.conf
 cat ipsec-vti.sh > /etc/ipsec-vti.sh
